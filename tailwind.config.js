edd79cd6f3fbcfb4a7c8c8945408499c35020f44
이동아 <christar99@naver.com>
2024-08-02 19:09:03 +0900

    개발환경 세팅(font, tailwind config, react-router 등)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+			width: {
+				"108": "27rem",
+				"120": "30rem",
+				"144": "36rem",
+			},
+			height: {
+				"108": "27rem",
+				"120": "30rem",
+				"144": "36rem",
+			},
+      fontFamily: {
+        Pretendard: ['Pretendard']
+      }
+    },
   },
   plugins: [],
 };
